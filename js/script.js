c87fd79a1ad96b760ee8a9aa9780736be5935873
tonmoybiswas9 <tonmoy.b3399@gmail.com>
2025-08-28 01:36:38 +0600

    added card js function and navbar funtionality

---
+++ js/script.js
@@ -0,0 +1,57 @@
+const heartIcon = document.getElementsByClassName("heart-icon");
+
+for (const icon of heartIcon) {
+  icon.addEventListener("click", function () {
+    const heartNumber = document.getElementById("heart-number").innerText;
+    console.log(heartNumber);
+    const totalOfHeartNumbers = Number(heartNumber) + 1;
+    document.getElementById("heart-number").innerText = totalOfHeartNumbers;
+  });
+}
+
+// call btn function
+const time = new Date().toLocaleTimeString();
+
+const callbtns = document.getElementsByClassName("call-button");
+for (const button of callbtns) {
+  const cardTitle = button.parentNode.parentNode.children[2].innerText;
+  const cardNumber = button.parentNode.parentNode.children[3].innerText;
+  button.addEventListener("click", function () {
+    const callingCoin = document.getElementById("calling-coin").innerText;
+    callingCoinConverted = parseInt(callingCoin);
+    const afterCallingPoint = callingCoinConverted - 20;
+    if (afterCallingPoint < 0) {
+      alert(
+        "❌You don't have enough coins. You need at least 20 coins to make a call."
+      );
+
+      return afterCallingPoint;
+    }
+
+    const updatedCoin = (document.getElementById("calling-coin").innerText =
+      afterCallingPoint);
+
+    alert("📞 Calling " + cardTitle + cardNumber + "...");
+
+    const historyContainer = document.getElementById("history-container");
+    const div = document.createElement("div");
+    div.innerHTML = `
+     <div
+                class="flex justify-between items-center bg-gray-100 p-3 mt-4 rounded-xl"
+              >
+                <div>
+                  <h2 class="font-bold mb-2">${cardTitle}</h2>
+                  <p> ${cardNumber} </p>
+                </div>
+                <p class="text-nowrap"> ${time} </span> AM</p>
+              </div>
+    `;
+    historyContainer.append(div);
+  });
+}
+
+// history section
+document.getElementById("clear-btn").addEventListener("click", function () {
+  const historyContainer = document.getElementById("history-container");
+  historyContainer.innerHTML = "";
+});
